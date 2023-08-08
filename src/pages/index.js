3b8eba813e98dda351d1d87a9f0b716e7fe63071
Nate <williamhamshar@gmail.com>
2023-08-07 21:29:00 -0400

    hours and map section

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,9 +56,34 @@ export default function Home() {
         <div className={styles.iceCreamBanner2}></div>
 
 
-        <div className={styles.aboutSection}>
-          
+        <div className={styles.storeDetailsSection}>
+          <h1 className={styles.storeHoursHeader}>Store Hours</h1>
+          <div className={styles.hoursDiv}>
+            <ul className={styles.dayList}>
+              <li className={styles.dayLI}>Sunday</li>
+              <li className={styles.dayLI}>Monday</li>
+              <li className={styles.dayLI}>Tuesday</li>
+              <li className={styles.dayLI}>Wednesday</li>
+              <li className={styles.dayLI}>Thursday</li>
+              <li className={styles.dayLI}>Friday</li>
+              <li className={styles.dayLI}>Saturday</li>
+            </ul>
+            <ul className={styles.hoursList}>
+              <li className={styles.hoursLI}>8:00 - 3:00</li>
+              <li className={styles.hoursLI}>7:00 - 5:00</li>
+              <li className={styles.hoursLI}>7:00 - 5:00</li>
+              <li className={styles.hoursLI}>7:00 - 8:00</li>
+              <li className={styles.hoursLI}>7:00 - 5:00</li>
+              <li className={styles.hoursLI}>7:00 - 5:00</li>
+              <li className={styles.hoursLI}>8:00 - 3:00</li>
+            </ul>
+          </div>
+          <div className={styles.locationDiv}>
+            <h1 className={styles.address}>90 Joshua Ln, Palmyra, VA 22963</h1>
+            <iframe className={styles.map} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3148.3770713380713!2d-78.31570492365849!3d37.898248205037135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b3ecbf6dbc22ef%3A0x625f304d86c8ab5b!2sCuppa%20Joe!5e0!3m2!1sen!2sus!4v1691457695711!5m2!1sen!2sus" width="350" height="350" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
         </div>
+        <div className={styles.placeholderDiv}></div>
       </main>
     </>
   )
