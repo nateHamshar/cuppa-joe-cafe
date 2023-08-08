be4142b338669c3c96d1a78891ac5819c89a1fe8
Nate <williamhamshar@gmail.com>
2023-08-08 17:42:38 -0400

    designing footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import styles from '../styles/footer.module.css'
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <h1 className={styles.footerHeader}>Cuppa Joe Cafe</h1>
+            <h1 className={styles.header}>Cuppa Joe Cafe</h1>
+            <a href="tel:4345910100" className={styles.number}>+1 (434) 591-0100</a>
+            <p className={styles.address}>90 Joshua Lane <br></br>Palmyra, VA 22963</p>
         </footer>
     );
 }
