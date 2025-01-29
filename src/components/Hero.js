d91ee28fb40be65fd86543febad11aa1a5f703c9
Nate Hamshar <williamhamshar@gmail.com>
2025-01-29 09:28:54 -0500

    changes to responsive hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,30 +9,32 @@ const Hero = () => {
         <section className={styles.landingDiv}>
            <div className={styles.heroImage}>
                 <Image
-                src="/cuppajoelobby.jpg"
+                src="/cuppaHero.jpg"
                 fill={true}
                 alt=''
+                priority={true}
+                className={styles.heroImageComponent}
                 />
-                
-                <div className={styles.imageOverlay}></div>
-                
-                <motion.h1 className={styles.heroTitle}
-                    initial={{ opacity: 0 }}
-                    whileInView={{ opacity: 1 }}
-                    transition={{ duration: 1.5 }}
-                >Cuppa Joe Cafe</motion.h1>
-                
-                <motion.div className={styles.heroCTADiv}
-                    initial={{ opacity: 0}}
-                    whileInView={{ opacity: 1}}
-                    transition={{ duration: 1.5 }}>
-
-                    <a href="tel:4345910100" className={styles.CTAfirst}>(434) 591-0100</a>
-
-                    <a href="/menu" className={styles.CTAsecond}>View Menu</a>
-
-                </motion.div>
             </div>
+            <div className={styles.imageOverlay}></div>
+            
+            <motion.h1 className={styles.heroTitle}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 1.5 }}
+            >Cuppa Joe Cafe
+            </motion.h1>
+            
+            <motion.div className={styles.heroCTADiv}
+                initial={{ opacity: 0}}
+                whileInView={{ opacity: 1}}
+                transition={{ duration: 1.5 }}>
+
+                <a href="tel:4345910100" className={styles.CTAfirst}>(434) 591-0100</a>
+
+                <a href="/menu" className={styles.CTAsecond}>View Menu</a>
+
+            </motion.div>
         
         </section>
     );
