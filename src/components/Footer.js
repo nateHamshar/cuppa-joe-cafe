92dc65c56a9689f44d0fca885882a2a0e9fab268
Nate Hamshar <106991008+nateHamshar@users.noreply.github.com>
2026-02-05 10:54:53 -0500

    update to new phone number

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className={styles.footer}>
             <h1 className={styles.header}>Cuppa Joe Cafe</h1>
-            <a href="tel:4345910100" className={styles.number}>+1 (434) 591-0100</a>
+            <a href="tel:4343732739" className={styles.number}>+1 (434) 373-2739</a>
             <p className={styles.address}>90 Joshua Lane <br></br>Palmyra, VA 22963</p>
         </footer>
     );
